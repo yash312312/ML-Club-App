72849bcc093d9234b7b09531414c2632f05460ce
yash312312 <yashsrivastava3@gmail.com>
2020-08-15 10:47:02 +0530

    Everything working

--- app/src/main/java/com/example/mlclub/ImportantArticlesFragment.java
+++ app/src/main/java/com/example/mlclub/ImportantArticlesFragment.java
@@ -11,33 +11,49 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class ImportantArticlesFragment extends Fragment {
     private ArrayList<String> mTitle = new ArrayList<>();
     private ArrayList<String> mLinks = new ArrayList<>();
-    View view;
+    private View view;
+    List<DocumentSnapshot> list;
+    FirebaseFirestore firebaseFirestore;
     private LinearLayoutManager mLayoutManager;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       view=inflater.inflate(R.layout.important_articles,container,false);
+        view = inflater.inflate(R.layout.important_articles, container, false);
+        firebaseFirestore = FirebaseFirestore.getInstance();
         mLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
         initList();
         return view;
     }
-    public void createImpArtRecyclerView(){
-        RecyclerView recyclerView=view.findViewById(R.id.imp_art_recycler_view);
-        ImportantArticleAdapter adapter=new ImportantArticleAdapter(mTitle,mLinks,getActivity().getApplicationContext());
+
+    private void createImpArtRecyclerView() {
+        RecyclerView recyclerView = view.findViewById(R.id.imp_art_recycler_view);
+        ImportantArticleAdapter adapter = new ImportantArticleAdapter(mTitle, mLinks, getActivity().getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setAdapter(adapter);
 
     }
-    public void initList(){
-        mLinks.add("www.google.com");
+
+    private void initList() {
+
+
+       /* mLinks.add("www.google.com");
         mLinks.add("www.Youtube.com");
         mTitle.add("Google");
-        mTitle.add("YouTube");
+        mTitle.add("YouTube");*/
         createImpArtRecyclerView();
 
     }
